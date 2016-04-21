94f6c5d469bbef895d9aa8509a36fa467c0e4f69
Molehant <marnik.molehant@accenture.com>
2016-04-21 11:56:19 +0300

    change code for basicdatasource to use h2 if no environment variable was given

--- src/main/java/com/example/MainConfig.java
+++ src/main/java/com/example/MainConfig.java
@@ -14,21 +14,19 @@ public class MainConfig {
 	public BasicDataSource dataSource() throws URISyntaxException {
 
 		URI dbUri = null;
-		
-		if (System.getenv("DATABASE_URL") == null) {
-			dbUri = new URI("postgres://dummy:dummy@dummy:2525/dummydb");
-		} else {
-			dbUri = new URI(System.getenv("DATABASE_URL"));
-		}
-
-		String username = dbUri.getUserInfo().split(":")[0];
-		String password = dbUri.getUserInfo().split(":")[1];
-		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
 		BasicDataSource basicDataSource = new BasicDataSource();
-		basicDataSource.setUrl(dbUrl);
-		basicDataSource.setUsername(username);
-		basicDataSource.setPassword(password);
+
+		if (System.getenv("DATABASE_URL") != null) {
+			dbUri = new URI(System.getenv("DATABASE_URL"));
+			String username = dbUri.getUserInfo().split(":")[0];
+			String password = dbUri.getUserInfo().split(":")[1];
+			String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+			basicDataSource.setUrl(dbUrl);
+			basicDataSource.setUsername(username);
+			basicDataSource.setPassword(password);
+		} else {
+			basicDataSource.setUrl("jdbc:h2:mem:test");
+		}
 
 		return basicDataSource;
 	}
