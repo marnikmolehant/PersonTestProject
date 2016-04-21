a028ab6a1bcd04378ac3e8bf24280b2c675d8406
Molehant <marnik.molehant@accenture.com>
2016-04-21 10:43:37 +0300

    add and change code for setting the BasicDataSource

--- src/main/java/com/example/MainConfig.java
+++ src/main/java/com/example/MainConfig.java
@@ -12,7 +12,14 @@ public class MainConfig {
 
 	@Bean
 	public BasicDataSource dataSource() throws URISyntaxException {
-		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+		URI dbUri = null;
+		
+		if (System.getenv("DATABASE_URL") == null) {
+			dbUri = new URI("postgres://dummy:dummy@dummy:2525/dummydb");
+		} else {
+			dbUri = new URI(System.getenv("DATABASE_URL"));
+		}
 
 		String username = dbUri.getUserInfo().split(":")[0];
 		String password = dbUri.getUserInfo().split(":")[1];
